351caadefcad9f5c54b10e69942dd9e734308ecb
RISH360 <mohamedrishan360@gmail.com>
2025-04-02 21:47:27 +0530

    Deleted all files

--- nearandfast.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll(".filter-btn:nth-child(2), .filter-btn:nth-child(3)"); 
-
-    buttons.forEach(button => {
-        button.addEventListener("click", function () {
-            button.classList.toggle("active-border"); // Toggle animation on click
-        });
-    });
-});
